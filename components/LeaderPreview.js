acf3d4e55bf021c864bc7996c035fda9bfbad450
Jason Xu <Xinwen31415@gmail.com>
2022-04-23 22:23:57 -0500

    added disable button and strikethrough text effect

--- components/LeaderPreview.js
+++ components/LeaderPreview.js
@@ -3,20 +3,23 @@ import { GlobalContext } from '../pages/_app';
 
 const LeaderPreview = (props) => {
 	const context = useContext(GlobalContext);
+	const disabled = context.disabledLeaders.includes(props.leader);
 
 	return (
-		<li
-			className={'civ' + (context.disabledLeaders.includes(props.leader) ? ' disabled' : '')}
-			onClick={() => {
-				context.dispatchLeader({
-					leader: props.leader,
-					type: context.disabledLeaders.includes(props.leader) ? 'REMOVE_LEADER' : 'ADD_LEADER'
-				});
-			}}>
+		<li className={'civ' + (disabled ? ' disabled' : '')}>
 			<img src={`/images/leader/${props.leader.replaceAll(' ', '_')}.png`} alt={props.leader + ' icon'} />
 			<div>
-				<h3>{props.leader}</h3>
-				{context.disabledLeaders.includes(props.leader) && <p>Disabled</p>}
+				<h3 className={disabled ? 'disabled' : ''}>{props.leader}</h3>
+				<button
+					className={(disabled ? 'enable' : 'disable') + '-btn'}
+					onClick={() => {
+						context.dispatchLeader({
+							leader: props.leader,
+							type: disabled ? 'REMOVE_LEADER' : 'ADD_LEADER'
+						});
+					}}>
+					{disabled ? 'Enable' : 'Disable'}
+				</button>
 			</div>
 		</li>
 	);
